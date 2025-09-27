06846912ae410cc945f442e5918734b730305912
David Broadlick <dbroadlick@gmail.com>
2025-09-26 19:45:35 -0500

    Adding password.

--- netlify/functions/ff2025.js
+++ netlify/functions/ff2025.js
@@ -1,14 +1,36 @@
-// netlify/functions/my-api-endpoint.js
+FF2025_LIST = [
+    "Reflection in a Dead Diamond",
+    "Ikarat Darah",
+    "Bulk",
+    "Secret 2: Bugonia",
+    "The Evil that Binds Us",
+    "Disforia",
+    "Tie Man",
+    "Night Patrol",
+    "Road to Vendetta",
+    "Whistle",
+]
+
 exports.handler = async (event, context) => {
   try {
-    return {
-      statusCode: 200,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify([
-        "test",
-        "test1",
-      ]),
-    };
+    const { password } = JSON.parse(event.body); // Example: getting data from request body
+    if(password && password == "reflection-in-a-dead-diamond") {
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify([
+                "test",
+                "test1",
+            ]),
+        };
+    }
+    else {
+        return {
+            statusCode: 401,
+            headers: { "Content-Type": "application/json" },
+            body: "Unauthorized"
+        };
+    }
   } catch (error) {
     return {
       statusCode: 500,
